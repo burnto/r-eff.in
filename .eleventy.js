6fda40402b930536a9200134eca714e9b38fea87
Brent Fitzgerald <burnto@gmail.com>
2020-07-28 22:57:23 -0700

    use expletives. add levels of severity

--- .eleventy.js
+++ .eleventy.js
@@ -76,6 +76,67 @@ module.exports = function (eleventyConfig) {
     "css", // css is not yet a recognized template extension in Eleventy
   ]);
 
+  eleventyConfig.addFilter("expletives", function (r) {
+    if (!r) {
+      return rand(
+        "possibly fucked but hard to say",
+        "probably best we don't know how fucked they are",
+        "if a county is fucked in the woods does it make a sound?",
+        "no data but plenty of jesus",
+        "doesnt know how fucked it is"
+      );
+    }
+    const n = Number(r);
+
+    if (n < 0.6) {
+      return rand(
+        "not at all fucked",
+        "fucking great",
+        "showing they have their shit together"
+      );
+    } else if (n < 0.8) {
+      return rand("not fucked", "pretty fucking fine", "not fucking bad");
+    } else if (n < 1) {
+      return rand(
+        "not the most fucked",
+        "only somewhat fucked",
+        "could be way more fucked",
+        "not completely fucked yet"
+      );
+    } else if (n < 1.1) {
+      return rand(
+        "fucked",
+        "not fucking good",
+        "increasingly fucked",
+        "looking fucked",
+        "getting pretty fucked",
+        "pretty fucking screwed"
+      );
+    } else if (n < 1.25) {
+      return rand(
+        "fucking fucked",
+        "pretty much fucked",
+        "fucking fucked up",
+        "so fucked",
+        "fucking screwed",
+        "fuckers aren't wearing masks",
+        "fuckers need to stay the fucked home"
+      );
+    } else {
+      return rand(
+        "so fucking fucked",
+        "totally fucked",
+        "fucking horrible",
+        "fucking fuck fucked",
+        "way way fucked"
+      );
+    }
+  });
+
+  eleventyConfig.addFilter("randomAngle", function (r) {
+    return Math.round(Math.random() * 10 - 5);
+  });
+
   eleventyConfig.addFilter("roundRt", function (r) {
     return Number(r).toFixed(2);
   });
@@ -85,14 +146,16 @@ module.exports = function (eleventyConfig) {
       return "unknown";
     }
     const n = Number(r);
-    if (n < 0.8) {
-      return "safe";
+    if (n < 0.6) {
+      return "verylow";
+    } else if (n < 0.8) {
+      return "low";
     } else if (n < 1) {
-      return "moderate";
+      return "medium";
     } else if (n < 1.1) {
-      return "risk";
+      return "high";
     } else if (n < 1.25) {
-      return "danger";
+      return "veryhigh";
     } else {
       return "severe";
     }
@@ -108,3 +171,8 @@ module.exports = function (eleventyConfig) {
   // You can return your Config object (optional).
   return {};
 };
+
+function rand() {
+  // "|" for a kinda "int div"
+  return arguments[(arguments.length * Math.random()) | 0];
+}
